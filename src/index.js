7e860f2d500b6b25ab9fffb8ab831dbb37192682
Ujjwal Kumar Das <ujjwaldas1059@gmail.com>
2023-05-06 15:21:12 +0530

    Fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import {browserHistory} from 'react-router-dom'
 
 import App from "./App";
+import store from "./app/store";
+
+
 
 ReactDOM.render(
-<Router><App/></Router>
+<Router>
+    <Provider store={store}>
+    <App/>
+    </Provider>
+ </Router>
 ,document.getElementById('root'))
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders={
+    'X-RapidAPI-Key': '8a352687a5msh5338b6e60e4ffc0p1d5c0fjsn327233432788',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl='https://coinranking1.p.rapidapi.com/coins'
+
+const createRequest = (url)=>({url,headers:cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints:(builder)=>({
+        getCryptos:builder.query({
+            query:()=>createRequest('/coins')
+        })
+    })
+})
+
+export const {useGetCryptosQuery} = createApi;
