959576f71afe398cae0b4c0462d75f988419fedf
Ujjwal Kumar Das <ujjwaldas1059@gmail.com>
2023-05-08 16:18:10 +0530

    update:Homepage and crypto-cur

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import {browserHistory} from 'react-router-dom'
-
 import App from "./App";
 import store from "./app/store";
-
-
 
 ReactDOM.render(
 <Router>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,7 +5,7 @@ const cryptoApiHeaders={
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 }
 
-const baseUrl='https://coinranking1.p.rapidapi.com/coins'
+const baseUrl='https://coinranking1.p.rapidapi.com'
 
 const createRequest = (url)=>({url,headers:cryptoApiHeaders})
 
@@ -14,9 +14,9 @@ export const cryptoApi = createApi({
     baseQuery:fetchBaseQuery({baseUrl}),
     endpoints:(builder)=>({
         getCryptos:builder.query({
-            query:()=>createRequest('/coins')
+            query:(count)=>createRequest(`/coins?limit=${count}`)
         })
     })
 })
 
-export const {useGetCryptosQuery} = createApi;
+export const {useGetCryptosQuery} = cryptoApi;
